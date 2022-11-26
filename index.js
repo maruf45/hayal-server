ff3961f715fd51ff13c3f9cf053b78f32a292d78
maruf45 <armaruf450@gmail.com>
2022-11-26 15:13:46 +0600

    add mongodb the serve

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require("express");
-require("dotenv").config();
 const app = express();
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fkczj.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const database = client.db('CarDatabase');
+  } catch (error) {
+    console.log(error);
+  }
+}
+run().catch((error) => console.log(error));
 
 app.get("/", (req, res) => {
   res.send("Server is running");
