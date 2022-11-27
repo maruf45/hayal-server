9e1d850d072223ebd74ff46ba20ecf8a6018e45a
maruf45 <armaruf450@gmail.com>
2022-11-27 10:38:06 +0600

    added car categories

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 
@@ -15,19 +15,29 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
-async function run() {
-  try {
-    const database = client.db('CarDatabase');
-  } catch (error) {
-    console.log(error);
-  }
-}
-run().catch((error) => console.log(error));
-
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
-app.listen((port) => {
-  console.log("server is running");
-});
+async function run() {
+    try {
+      const database = client.db('CarDatabase');
+      const carCollection = database.collection('UsedCars');
+      const carCategories = database.collection('CarCategories');
+      app.get('/userdCars', async (req, res) => {
+        const query = {};
+        const result = await carCollection.find(query).toArray();
+        res.send(result)
+      })
+      app.get('/carCategories', async(req, res) => {
+        const query = {};
+        const result = await carCategories.find(query).toArray();
+        res.send(result)
+      })
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  run().catch((error) => console.log(error.message));
+  
+app.listen(port)
