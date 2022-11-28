f245546f442e787db4572b7a71d7c041e966614c
maruf45 <armaruf450@gmail.com>
2022-11-28 19:17:33 +0600

    added blogs

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
+const { query } = require("express");
 
 // midlleWare
 app.use(cors());
@@ -17,20 +18,18 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 function verifyToken(req, res, next) {
-    const authHeader = req.headers.authorization;
-    if(!authHeader){
-        return  res.status(401).send('unauthorized acces')
-
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "unauthorize access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "forbidden acces" });
     }
-    const token = authHeader.split(' ')[2];
-    jwt.verfy(token, process.env.ACCES_TOKEN, function(err, decoded){
-        if(err){
-            return res.status(403).send({message: 'forbidden acces'})
-        }
-        req.decoded = decoded;
-        next()
-    })
-
+    req.decoded = decoded;
+    next();
+  });
 }
 app.get("/", (req, res) => {
   res.send("Server is running");
@@ -48,16 +47,22 @@ async function run() {
       const query = { email: email };
       const user = await userCollection.findOne(query);
       if (user) {
-        const token = jwt.sign({ email }, process.env.ACCES_TOKEN, {
-          expiresIn: "1d",
+        const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, {
+          expiresIn: "15h",
         });
-        return res.send({ accesToken: token });
+        return res.send({ accessToken: token });
       }
-      res.status(403).send({ accesToken: "" });
+      res.status(403).send({ accessToken: "" });
     });
     app.get("/userdCars", async (req, res) => {
       const query = {};
       const result = await carCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.post("/userdCars", async (req, res) => {
+      const data = req.body;
+      console.log(data);
+      const result = await carCollection.insertOne(data);
       res.send(result);
     });
     app.get("/carCategories", async (req, res) => {
@@ -76,21 +81,73 @@ async function run() {
       const result = await userOrderCollection.insertOne(data);
       res.send(result);
     });
-    app.get("/userOrders",verifyToken, async (req, res) => {
+    app.get("/userOrders", verifyToken, async (req, res) => {
       const email = req.query.email;
       const decoded = req.decoded.email;
-      if(email !== decoded){
-        return res.status(403).send({massgae: 'forbidden acces'})
+
+      if (email !== decoded) {
+        return res.status(403).send({ message: "forbidden access" });
+      } else {
+        const query = { email: email };
+        const result = await userOrderCollection.find(query).toArray();
+        res.send(result);
       }
-      const query = { email: email };
-      const result = await userOrderCollection.find(query).toArray();
-      res.send(result);
     });
     app.post("/users", async (req, res) => {
       const user = req.body;
-      console.log(user);
+
       const result = await userCollection.insertOne(user);
       res.send(result);
+    });
+    app.put("/user/seller/", verifyToken, async (req, res) => {
+      const email = req.query.email;
+      const userType = req.query.userType;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          userType: userType,
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+    });
+    app.get("/allUser", async (req, res) => {
+      const userType = req.query.userType;
+      if (userType === userType) {
+        const query = { userType: userType };
+        const allUser = await userCollection.find(query).toArray();
+        res.send(allUser);
+      } else if (userType === userType) {
+        const query = { userType: userType };
+        const allUser = await userCollection.find(query).toArray();
+        res.send(allUser);
+      }
+    });
+    app.get("/user/seller/", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      res.send({ isSeller: user?.userType === "seller" });
+    });
+    // app.put("/user/admin/", verifyToken, async (req, res) => {
+    //   const email = req.query.email;
+    //   const role = req.query.role;
+    //   const filter = { email: email };
+    //   const options = { upsert: true };
+    //   const updateDoc = {
+    //     $set: {
+    //       role: role,
+    //     },
+    //   };
+    //   const result = await userCollection.updateOne(filter, updateDoc, options);
+    //   res.send(result);
+    // });
+    app.get("/user/admin/", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      res.send({ isAdmin: user?.role === "admin" });
     });
   } catch (error) {
     console.log(error);
